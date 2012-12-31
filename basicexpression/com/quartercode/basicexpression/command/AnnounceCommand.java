93dcc850cfc0adf060088051dbe7417912d5a3c6
TutorialMakerHD <info@tutorialmakerhd.de>
2012-12-31 13:11:34 +0100

    Color Code Support - Announce Expression, Control
    We have added color code support in the Announce Expression & Control.

--- basicexpression/com/quartercode/basicexpression/command/AnnounceCommand.java
+++ basicexpression/com/quartercode/basicexpression/command/AnnounceCommand.java
@@ -28,7 +28,7 @@ public class AnnounceCommand implements ExpressionCommand {
     public void execute(final Minecart minecart, final Object parameter) {
 
         if (parameter != null) {
-            ((Player) minecart.getPassenger()).sendMessage(String.valueOf(parameter));
+            ((Player) minecart.getPassenger()).sendMessage(String.valueOf(parameter).replaceAll("&0", "§0").replaceAll("&1", "§1").replaceAll("&2", "§2").replaceAll("&3", "§3").replaceAll("&4", "§4").replaceAll("&5", "§5").replaceAll("&6", "§6").replaceAll("&7", "§7").replaceAll("&8", "§8").replaceAll("&9", "§9").replaceAll("&a", "§a").replaceAll("&b", "§b").replaceAll("&c", "§c").replaceAll("&d", "§d").replaceAll("&e", "§e").replaceAll("&f", "§f").replaceAll("&k", "§k").replaceAll("&l", "§l").replaceAll("&n", "§n").replaceAll("&o", "§o").replaceAll("&r", "§r"));
         }
     }
 
