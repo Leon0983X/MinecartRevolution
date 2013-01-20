c4658a1376185bd1b582794c6785f4274c094c4c
LoadingByte <loadingbyte@gmail.com>
2013-01-20 20:58:03 +0100

    Chest Dispenser Patch
    We cleaned and patched the code of the chest dispenser.

--- basicactions/com/quartercode/basicactions/listener/BlockListener.java
+++ basicactions/com/quartercode/basicactions/listener/BlockListener.java
@@ -4,18 +4,16 @@ package com.quartercode.basicactions.listener;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockState;
 import org.bukkit.block.Chest;
-import org.bukkit.block.Sign;
 import org.bukkit.entity.Minecart;
-import org.bukkit.entity.PoweredMinecart;
-import org.bukkit.entity.StorageMinecart;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockRedstoneEvent;
-import org.bukkit.util.Vector;
+import org.bukkit.inventory.ItemStack;
+import com.quartercode.basicexpression.util.Direction;
 import com.quartercode.minecartrevolution.MinecartRevolution;
+import com.quartercode.minecartrevolution.util.MinecartType;
+import com.quartercode.minecartrevolution.util.MinecartUtil;
 
 public class BlockListener implements Listener {
 
@@ -27,153 +25,67 @@ public class BlockListener implements Listener {
     @EventHandler
     public void onRedstoneBlockChange(final BlockRedstoneEvent event) {
 
-        final Block block = event.getBlock();
-        Location newLocation = block.getLocation();
-
-        newLocation.setX(newLocation.getX() + 1.0D);
-        if (newLocation.getBlock().getType() == Material.CHEST || newLocation.getBlock().getType() == Material.SIGN_POST) {
-            if (block.getData() >= 9) {
-                return;
-            }
-            tryDispenseMinecart(newLocation.getBlock());
+        if (event.getBlock().getData() >= 9) {
             return;
         }
-        newLocation = block.getLocation();
-        newLocation.setX(newLocation.getX() - 1.0D);
-        if (newLocation.getBlock().getType() == Material.CHEST || newLocation.getBlock().getType() == Material.SIGN_POST) {
-            if (block.getData() >= 9) {
-                return;
-            }
-            tryDispenseMinecart(newLocation.getBlock());
+
+        Chest chest;
+        Location location = event.getBlock().getLocation();
+        if (location.add(1, 0, 0).getBlock().getType() == Material.CHEST) {
+            chest = (Chest) location.getBlock().getState();
+        } else if (location.add(-1, 0, 0).getBlock().getType() == Material.CHEST) {
+            chest = (Chest) location.getBlock().getState();
+        } else if (location.add(0, 0, 1).getBlock().getType() == Material.CHEST) {
+            chest = (Chest) location.getBlock().getState();
+        } else if (location.add(0, 0, -1).getBlock().getType() == Material.CHEST) {
+            chest = (Chest) location.getBlock().getState();
+        } else {
             return;
         }
-        newLocation = block.getLocation();
-        newLocation.setZ(newLocation.getZ() + 1.0D);
-        if (newLocation.getBlock().getType() == Material.CHEST || newLocation.getBlock().getType() == Material.SIGN_POST) {
-            if (block.getData() >= 9) {
-                return;
-            }
-            tryDispenseMinecart(newLocation.getBlock());
+
+        Location rails;
+        Direction direction;
+        location = chest.getLocation();
+        if (location.add(1, 0, 0).getBlock().getType() == Material.RAILS || location.add(1, 0, 0).getBlock().getType() == Material.POWERED_RAIL || location.add(1, 0, 0).getBlock().getType() == Material.DETECTOR_RAIL) {
+            rails = location;
+            direction = Direction.WEST;
+        } else if (location.add(-1, 0, 0).getBlock().getType() == Material.RAILS || location.add(-1, 0, 0).getBlock().getType() == Material.POWERED_RAIL || location.add(-1, 0, 0).getBlock().getType() == Material.DETECTOR_RAIL) {
+            rails = location;
+            direction = Direction.EAST;
+        } else if (location.add(0, 0, 1).getBlock().getType() == Material.RAILS || location.add(0, 0, 1).getBlock().getType() == Material.POWERED_RAIL || location.add(0, 0, 1).getBlock().getType() == Material.DETECTOR_RAIL) {
+            rails = location;
+            direction = Direction.NORTH;
+        } else if (location.add(0, 0, -1).getBlock().getType() == Material.RAILS || location.add(0, 0, -1).getBlock().getType() == Material.POWERED_RAIL || location.add(0, 0, -1).getBlock().getType() == Material.DETECTOR_RAIL) {
+            rails = location;
+            direction = Direction.SOUTH;
+        } else {
             return;
         }
-        newLocation = block.getLocation();
-        newLocation.setZ(newLocation.getZ() - 1.0D);
-        if (newLocation.getBlock().getType() == Material.CHEST || newLocation.getBlock().getType() == Material.SIGN_POST) {
-            if (block.getData() >= 9) {
-                return;
-            }
-            tryDispenseMinecart(newLocation.getBlock());
-            return;
-        }
-    }
 
-    private void tryDispenseMinecart(final Block block) {
+        MinecartType minecartType = null;
+        for (int slot = 0; slot < chest.getInventory().getSize(); slot++) {
+            if (chest.getInventory().getItem(slot) != null && chest.getInventory().getItem(slot).getType() != Material.AIR) {
+                final Material type = chest.getInventory().getItem(slot).getType();
 
-        final BlockState state = block.getState();
-
-        String type = "";
-        Sign sign = null;
-        Chest chest = null;
-        if (state.getType() == Material.SIGN_POST) {
-            sign = (Sign) state;
-            type = "SIGN";
-        } else if (state instanceof Chest) {
-            chest = (Chest) state;
-            type = "CHEST";
-        }
-
-        Location newLocation = block.getLocation();
-        Location newBlockLocation = block.getLocation();
-        newBlockLocation.setY(newBlockLocation.getY() - 2.0D);
-        if (newBlockLocation.getBlock().getType() == Material.RAILS) {
-            newLocation = newBlockLocation;
-        }
-        newBlockLocation = block.getLocation();
-        newBlockLocation.setY(newBlockLocation.getY() + 2.0D);
-        if (newBlockLocation.getBlock().getType() == Material.RAILS) {
-            newLocation = newBlockLocation;
-        }
-        newBlockLocation = block.getLocation();
-        newBlockLocation.setY(newBlockLocation.getY() + 3.0D);
-        if (newBlockLocation.getBlock().getType() == Material.RAILS) {
-            newLocation = newBlockLocation;
-        }
-
-        newLocation.setX(newLocation.getX() + 1.0D);
-        if (newLocation.getBlock().getType() == Material.RAILS) {
-            dispenseMinecart(chest, sign, type, newLocation.getBlock(), new Vector(1, 0, 0));
-            return;
-        }
-        newLocation.setX(newLocation.getX() - 2.0D);
-        if (newLocation.getBlock().getType() == Material.RAILS) {
-            dispenseMinecart(chest, sign, type, newLocation.getBlock(), new Vector(-1, 0, 0));
-            return;
-        }
-        newLocation.setX(newLocation.getX() + 1.0D);
-        newLocation.setZ(newLocation.getZ() + 1.0D);
-        if (newLocation.getBlock().getType() == Material.RAILS) {
-            dispenseMinecart(chest, sign, type, newLocation.getBlock(), new Vector(0, 0, 1));
-            return;
-        }
-        newLocation.setZ(newLocation.getZ() - 2.0D);
-        if (newLocation.getBlock().getType() == Material.RAILS) {
-            dispenseMinecart(chest, sign, type, newLocation.getBlock(), new Vector(0, 0, -1));
-            return;
-        }
-    }
-
-    private void dispenseMinecart(final Chest chest, final Sign sign, final String type, final Block railBlock, final Vector velocity) {
-
-        int minecartType = -1;
-
-        if (type.equalsIgnoreCase("SIGN")) {
-            if (sign.getLine(1).equalsIgnoreCase("[Spawn]")) {
-                if (sign.getLine(2).equalsIgnoreCase("Standard")) {
-                    minecartType = 0;
-                } else if (sign.getLine(2).equalsIgnoreCase("Storage")) {
-                    minecartType = 1;
-                } else if (sign.getLine(2).equalsIgnoreCase("Powered")) {
-                    minecartType = 2;
-                } else {
-                    minecartType = 0;
+                if (type == Material.MINECART) {
+                    minecartType = MinecartType.MINECART;
+                    chest.getInventory().setItem(slot, new ItemStack(Material.AIR));
+                    break;
+                } else if (type == Material.STORAGE_MINECART) {
+                    minecartType = MinecartType.STORAGE;
+                    chest.getInventory().setItem(slot, new ItemStack(Material.AIR));
+                    break;
+                } else if (type == Material.POWERED_MINECART) {
+                    minecartType = MinecartType.POWERED;
+                    chest.getInventory().setItem(slot, new ItemStack(Material.AIR));
+                    break;
                 }
             }
-        } else if (type.equalsIgnoreCase("CHEST")) {
-            int firstMinecartSlot = chest.getInventory().first(Material.MINECART);
-            int firstStorageMinecartSlot = chest.getInventory().first(Material.STORAGE_MINECART);
-            int firstPoweredMinecartSlot = chest.getInventory().first(Material.POWERED_MINECART);
-
-            if (firstMinecartSlot < 0) {
-                firstMinecartSlot = 100;
-            }
-            if (firstStorageMinecartSlot < 0) {
-                firstStorageMinecartSlot = 100;
-            }
-            if (firstPoweredMinecartSlot < 0) {
-                firstPoweredMinecartSlot = 100;
-            }
-
-            if (firstStorageMinecartSlot < firstMinecartSlot && firstStorageMinecartSlot < firstPoweredMinecartSlot) {
-                minecartType = 1;
-                chest.getInventory().setItem(chest.getInventory().first(Material.STORAGE_MINECART), null);
-            } else if (firstPoweredMinecartSlot < firstMinecartSlot && firstPoweredMinecartSlot < firstStorageMinecartSlot) {
-                minecartType = 2;
-                chest.getInventory().setItem(chest.getInventory().first(Material.POWERED_MINECART), null);
-            } else if (firstMinecartSlot < firstStorageMinecartSlot && firstMinecartSlot < firstPoweredMinecartSlot) {
-                minecartType = 0;
-                chest.getInventory().setItem(chest.getInventory().first(Material.MINECART), null);
-            }
         }
 
-        if (minecartType == 0) {
-            final Minecart minecart = railBlock.getWorld().spawn(railBlock.getLocation(), Minecart.class);
-            minecart.setVelocity(velocity);
-        } else if (minecartType == 1) {
-            final Minecart minecart = railBlock.getWorld().spawn(railBlock.getLocation(), StorageMinecart.class);
-            minecart.setVelocity(velocity);
-        } else if (minecartType == 2) {
-            final Minecart minecart = railBlock.getWorld().spawn(railBlock.getLocation(), PoweredMinecart.class);
-            minecart.setVelocity(velocity);
+        if (minecartType != null) {
+            final Minecart minecart = location.getWorld().spawn(rails, minecartType.getCartClass());
+            MinecartUtil.driveInDirection(minecart, direction);
         }
     }
 
